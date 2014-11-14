0ead7a0a22f41f193a5bdbf78aec5f83b434fb6d
clynchga <clarevlynch@gmail.com>
2014-11-13 21:59:12 -0500

    basic search form structure

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  get '/' => 'requests#new'
+  resources :requests, only: [:create, :new, :index]
 
   get '/example' => 'pages#textsearch'
 
