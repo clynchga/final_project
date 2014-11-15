c656d67104968ce6f0fc7ae36b41c1d4bc7ed8e3
clynchga <clarevlynch@gmail.com>
2014-11-14 21:45:03 -0500

    ugly but functional request form

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ActiveRecord::Base
-	validates :year1, :year2, presence: true
-	validates :year1, :year2, :month1, :month2, :day1, :day2, numericality: { only_integer: true }
-	validates :year1, :year2, numericality: { greater_than_or_equal_to: 1836, less_than_or_equal_to: 1922 }
-	validates :month1, :month2, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
-	validates :day1, :day2, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
+	#validates :year1, :year2, presence: true
+	#validates :year1, :year2, :month1, :month2, :day1, :day2, numericality: { only_integer: true }
+	#validates :year1, :year2, numericality: { greater_than_or_equal_to: 1836, less_than_or_equal_to: 1922 }
+	#validates :month1, :month2, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
+	#validates :day1, :day2, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
 end
