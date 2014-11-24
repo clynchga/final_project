cb15f564315f20880f64eeb0eb0f56a720dbc2de
clynchga <clarevlynch@gmail.com>
2014-11-24 09:59:20 -0500

    added build_url method to Requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,9 +5,8 @@ def new
 end
 
 def create
-	base_url = "http://chroniclingamerica.loc.gov/search/pages/results/?dateFilterType=range"
 	@request = Request.new(request_params)
-
+	@request.url = Request.build_url(@request.year1, @request.month1, @request.day1, @request.year2, @request.month2, @request.day2, @request.andtext, @request.ortext, @request.phrasetext, @request.state, @request.lccn)
 	respond_to do |format|
 		if @request.save
 	   		format.html { redirect_to requests_path, notice: 'Saved request to db' }
@@ -22,6 +21,12 @@ end
 def index
 	@requests = Request.all
 end 
+
+def self.build_url(year1, month1, day1, year2, month2, day2, andtext, ortext, phrasetext, state, lccn)
+	base_url = "http://chroniclingamerica.loc.gov/search/pages/results/?dateFilterType=range"
+	url = base_url + "&date1=#{month1}/#{day1}/#{year1}&date2=#{month2}/#{day2}/#{year2}&andtext=#{andtext}&ortext=#{ortext}&phrasetext=#{phrasetext}&state=#{state}&lccn=#{lccn}"
+	url
+end
 
 private
 def request_params
