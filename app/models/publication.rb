88299dfafc5ee3957f4c1ca6ee79e36fb0be8812
clynchga <clarevlynch@gmail.com>
2014-11-26 14:52:35 -0500

    validation of lccn uniqueness for publication model; form tweaks

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,5 +1,6 @@
 class Publication < ActiveRecord::Base
 	has_many :pages
+	validates :lccn, :uniqueness => true
 
 	def self.find_id_by_lccn(lccn)
 		@publication = Publication.where(lccn: lccn).first
