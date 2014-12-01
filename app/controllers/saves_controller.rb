d516b5bab8a8ea474398d1ad7def2da13639f30d
clynchga <clarevlynch@gmail.com>
2014-11-30 19:43:34 -0500

    saved pages done

--- app/controllers/saves_controller.rb
+++ app/controllers/saves_controller.rb
@@ -3,6 +3,13 @@ class SavesController < ApplicationController
 before_action :authenticate_user!
 
 def index
+
+	user_id = current_user.id
+	@save_ids = Save.where(user_id: user_id).distinct.pluck(:page_id)
+	# returns an array of ids, or an empty array
+
+	@pages = Page.find(@save_ids)
+	# returns an array of page objects, or an empty array
 end
 
 def create
