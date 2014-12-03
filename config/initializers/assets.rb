e482843809607db6be420f9a4f426dd8c85b2c25
clynchga <clarevlynch@gmail.com>
2014-12-02 19:28:20 -0500

    db error that was stopping heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 # Rails.application.config.assets.precompile += %w( forms.css )
 
-Rails.application.config.assets.precompile += %w( pages.css requests.css saves.css)
+# Rails.application.config.assets.precompile += %w( pages.css requests.css saves.css)
--- db/migrate/20141115021258_remove_year1_from_requests.rb
+++
@@ -1,5 +0,0 @@
-class RemoveYear1FromRequests < ActiveRecord::Migration
-  def change
-    remove_column :requests, :year1, :text
-  end
-end
